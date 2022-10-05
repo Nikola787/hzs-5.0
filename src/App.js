bae6697d80a7a46dcfd6b7b586747220ce912136
nikola787 <ratkovicnikola64@gmail.com>
2022-10-06 01:27:56 +0200

    tajmer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
-import "./App.css";
-=======
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './Home';
->>>>>>> 41d79da4d7fb7e029caa7e7815652067f940b905
 
 function App() {
   return (
--- src/index.js
+++ src/index.js
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-=======
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -14,7 +5,6 @@ import App from './App';
 import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
->>>>>>> 41d79da4d7fb7e029caa7e7815652067f940b905
   <React.StrictMode>
     <HashRouter basename="/">
       <App />
