12df627e57786efe958ce89437b329b11c3c7cbc
nikola787 <ratkovicnikola64@gmail.com>
2022-10-06 16:24:20 +0200

    master

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Animacija from './components/animacija/animacija.js'
 import Tajmer from './components/tajmer/tajmer.jsx'
+import Radionice from './components/radionice/radionice.js'
 
 const Home = () => {
   return (
