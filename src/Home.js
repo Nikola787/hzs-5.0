9ec3ff2cb7a3838f3a061028e58b773eca86807e
IlijaTrifke <gtrifunovic05@gmail.com>
2022-10-06 16:06:04 +0200

    proba sa slikom i tekstom

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Animacija from "./components/animacija/animacija.js";
 import Tajmer from "./components/tajmer/tajmer.jsx";
-import Radionice from "./components/radionice/radionice.js";
+import Radionice from "./components/radionice/radionice.jsx";
 
 const Home = () => {
-  return <div>Test</div>;
+  return (
+    <div>
+      <Radionice />
+    </div>
+  );
 };
 
 export default Home;
