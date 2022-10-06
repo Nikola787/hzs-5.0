4352c424621823d08a34a2f5fb7c0ddcbb1d570b
IlijaTrifke <gtrifunovic05@gmail.com>
2022-10-06 11:29:25 +0200

    npm i, npm i react-router-dom

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,10 @@
-import React from 'react'
-import Animacija from './components/animacija/animacija.js'
-import Tajmer from './components/tajmer/tajmer.jsx'
+import React from "react";
+import Animacija from "./components/animacija/animacija.js";
+import Tajmer from "./components/tajmer/tajmer.jsx";
+import Radionice from "./components/radionice/radionice.js";
 
 const Home = () => {
-  return (
-    <div>
-      Test
-    </div>
-  )
-}
+  return <div>Test</div>;
+};
 
-export default Home
+export default Home;
