586f4e9a305549dd37728626ae8f3d62f3016e39
petar-graovac <petargraovac01@gmail.com>
2022-10-08 17:31:33 +0200

    naslov

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,14 @@
-import React from 'react'
-import Animacija from './components/animacija/animacija.js'
-import Tajmer from './components/tajmer/tajmer.jsx'
+import React from "react";
+import Animacija from "./components/animacija/animacija.js";
+import Tajmer from "./components/tajmer/tajmer.jsx";
+import O_projektu from "./components/o_projektu/o_projektu.js";
 
 const Home = () => {
   return (
     <div>
-      Test
+      <O_projektu />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
---
+++ src/components/o_projektu/o_projektu.js
@@ -0,0 +1,53 @@
+import React from "react";
+import "./o_projektu.css";
+
+const o_projektu = () => {
+  return (
+    <div className="ceo">
+      <div className="ceo_naslov">
+        <div id="rectangle"></div>
+        <div id="tekst_naslov">O PROJEKTU</div>
+      </div>
+      <div className="telo">
+        <div className="tekst">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
+          perferendis facere odio! Eius omnis, iste dicta numquam ad dolor
+          dignissimos error ea, necessitatibus, veritatis enim obcaecati nisi
+          tempora tenetur? Neque.
+          <br />
+          <br />
+          Optio facere repudiandae nam iusto nobis? Ad voluptatem corrupti
+          consequuntur odit beatae aperiam voluptas tempora illum, eveniet
+          facere repellendus asperiores accusamus nam magnam quo cum quam,
+          nostrum, reprehenderit molestiae saepe. Perspiciatis eum nihil et
+          accusamus hic repellendus nobis provident alias ea doloribus beatae,
+          cumque, autem laborum.
+          <br />
+          <br />
+          Magnam animi neque impedit iure illo nobis, saepe minima ut
+          necessitatibus distinctio consequatur amet? Optio facere repudiandae
+          nam iusto nobis? Ad voluptatem corrupti consequuntur odit beatae
+          aperiam voluptas tempora illum, eveniet facere repellendus asperiores
+          accusamus nam magnam quo cum quam, nostrum, reprehenderit molestiae
+          saepe. Perspiciatis eum nihil et accusamus hic repellendus nobis
+          provident alias ea doloribus beatae, cumque, autem laborum.
+          <br />
+          <br />
+          Optio facere repudiandae nam iusto nobis? Ad voluptatem corrupti
+          consequuntur odit beatae aperiam voluptas tempora illum, eveniet
+          facere repellendus asperiores accusamus nam magnam quo cum quam,
+          nostrum, reprehenderit molestiae saepe. Perspiciatis eum nihil et
+          accusamus hic repellendus nobis provident alias ea doloribus beatae,
+          cumque, autem laborum.
+        </div>
+        <div className="statistika">
+          <div className="timovi"></div>
+          <div className="gradovi"></div>
+          <div className="takmicari"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default o_projektu;
