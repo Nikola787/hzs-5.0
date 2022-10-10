760ef49d88e483e3903bbd163d206be433e16ab4
nikola787 <ratkovicnikola64@gmail.com>
2022-10-10 11:44:30 +0200

    dodata forma za clanove

--- src/Home.js
+++ src/Home.js
@@ -2,11 +2,12 @@ import React from "react";
 import Animacija from "./components/animacija/animacija.js";
 import Tajmer from "./components/tajmer/tajmer.jsx";
 import Radionice from "./components/radionice/radionice.jsx";
+import Prijava from "./components/prijava/prijava.js"
 
 const Home = () => {
   return (
     <div>
-      <Radionice />
+      <Prijava />
     </div>
   );
 };
---
+++ src/components/prijava/prijava.js
@@ -0,0 +1,338 @@
+import React from 'react'
+import './prijava.css'
+import Ikona1 from './assets/1.png'
+import Ikona2 from './assets/2.png'
+import Ikona3 from './assets/3.png'
+import Ikona4 from './assets/4.png'
+import Ikona5 from './assets/5.png'
+import Ikona6 from './assets/6.png'
+
+
+
+const prijava = () => {
+  return (
+    <div>
+        <div className="ceo">
+          <div className="naslov">
+            <div className="boja"></div>
+            <div className="naslov-tekst">FORMA ZA PRIJAVU</div>
+          </div>
+            <div className="forma">
+              <div className="clan">
+                1. Član
+              </div>
+              <div className="box-clan">
+                <div className="input">
+                <div className="unos">
+                  <div className="unos-ikona">
+                    <img src={Ikona6} alt="Ikona1" height={'50px'} />
+                  </div>
+                  <div className="unos-tekst">
+                   <label>
+                    <input type="text" placeholder="Ime i prezime"/>
+                   </label>
+                  </div>
+                </div>
+
+                <div className="unos">
+                  <div className="unos-ikona">
+                    <img src={Ikona2} alt="Ikona1" height={'50px'} />
+                  </div>
+                  <div className="unos-tekst">
+                    <label>
+                    <input type="text" placeholder="Grad"/>
+                    </label>
+                  </div>
+                </div>
+
+                <div className="unos">
+                  <div className="unos-ikona">
+                    <img src={Ikona1} alt="Ikona1" height={'50px'} />
+                  </div>
+                  <div className="unos-tekst">
+                    <label>
+                    <input type="text" placeholder="Srednja škola"/>
+                    </label>
+                  </div>
+                </div>
+
+                <div className="unos">
+                  <div className="unos-ikona">
+                    <img src={Ikona5} alt="Ikona1" height={'50px'} />
+                  </div>
+                  <div className="unos-tekst">
+                   <label>
+                   <input type="text" placeholder="Razred"/>
+                   </label>
+                  </div>
+                </div>
+                <div className="unos">
+                  <div className="unos-ikona">
+                    <img src={Ikona4} alt="Ikona1" height={'50px'} />
+                  </div>
+                  <div className="unos-tekst">
+                   <label>
+                   <input type="text" placeholder="Broj telefona"/>
+                   </label>
+                  </div>
+                </div>
+                <div className="unos">
+                  <div className="unos-ikona">
+                    <img src={Ikona3} alt="Ikona1" height={'50px'} />
+                  </div>
+                  <div className="unos-tekst">
+                   <label>
+                   <input type="text" placeholder="E-mail"/>
+                   </label>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="forma">
+              <div className="clan">
+                  2. Član
+              </div>
+              <div className="box-clan">
+                <div className="input">
+                  <div className="unos">
+                    <div className="unos-ikona">
+                      <img src={Ikona6} alt="Ikona1" height={'50px'} />
+                    </div>
+                    <div className="unos-tekst">
+                      <label>
+                      <input type="text" placeholder="Ime i prezime"/>
+                      </label>
+                    </div>
+                  </div>
+                  <div className="unos">
+                  <div className="unos-ikona">
+                    <img src={Ikona2} alt="Ikona1" height={'50px'} />
+                  </div>
+                  <div className="unos-tekst">
+                    <label>
+                    <input type="text" placeholder="Grad"/>
+                    </label>
+                  </div>
+                </div>
+                <div className="unos">
+                  <div className="unos-ikona">
+                    <img src={Ikona1} alt="Ikona1" height={'50px'} />
+                  </div>
+                  <div className="unos-tekst">
+                    <label>
+                    <input type="text" placeholder="Srednja škola"/>
+                    </label>
+                  </div>
+                </div>
+                <div className="unos">
+                  <div className="unos-ikona">
+                    <img src={Ikona5} alt="Ikona1" height={'50px'} />
+                  </div>
+                  <div className="unos-tekst">
+                   <label>
+                   <input type="text" placeholder="Razred"/>
+                   </label>
+                  </div>
+                </div>
+                <div className="unos">
+                  <div className="unos-ikona">
+                    <img src={Ikona4} alt="Ikona1" height={'50px'} />
+                  </div>
+                  <div className="unos-tekst">
+                   <label>
+                   <input type="text" placeholder="Broj telefona"/>
+                   </label>
+                  </div>
+                </div>
+                <div className="unos">
+                  <div className="unos-ikona">
+                    <img src={Ikona3} alt="Ikona1" height={'50px'} />
+                  </div>
+                  <div className="unos-tekst">
+                   <label>
+                   <input type="text" placeholder="E-mail"/>
+                   </label>
+                  </div>
+                </div>         
+                </div>
+              </div>
+            </div>
+
+            <div className="forma">
+              <div className="clan">
+                  3. Član
+              </div>
+              <div className="box-clan">
+                <div className="input">
+                  <div className="unos">
+                    <div className="unos-ikona">
+                      <img src={Ikona6} alt="Ikona1" height={'50px'} />
+                    </div>
+                    <div className="unos-tekst">
+                      <label>
+                      <input type="text" placeholder="Ime i prezime"/>
+                      </label>
+                    </div>
+                  </div>
+                  <div className="unos">
+                  <div className="unos-ikona">
+                    <img src={Ikona2} alt="Ikona1" height={'50px'} />
+                  </div>
+                  <div className="unos-tekst">
+                    <label>
+                    <input type="text" placeholder="Grad"/>
+                    </label>
+                  </div>
+                </div>
+                <div className="unos">
+                  <div className="unos-ikona">
+                    <img src={Ikona1} alt="Ikona1" height={'50px'} />
+                  </div>
+                  <div className="unos-tekst">
+                    <label>
+                    <input type="text" placeholder="Srednja škola"/>
+                    </label>
+                  </div>
+                </div>
+                <div className="unos">
+                  <div className="unos-ikona">
+                    <img src={Ikona5} alt="Ikona1" height={'50px'} />
+                  </div>
+                  <div className="unos-tekst">
+                   <label>
+                   <input type="text" placeholder="Razred"/>
+                   </label>
+                  </div>
+                </div>
+                <div className="unos">
+                  <div className="unos-ikona">
+                    <img src={Ikona4} alt="Ikona1" height={'50px'} />
+                  </div>
+                  <div className="unos-tekst">
+                   <label>
+                   <input type="text" placeholder="Broj telefona"/>
+                   </label>
+                  </div>
+                </div>
+                <div className="unos">
+                  <div className="unos-ikona">
+                    <img src={Ikona3} alt="Ikona1" height={'50px'} />
+                  </div>
+                  <div className="unos-tekst">
+                   <label>
+                   <input type="text" placeholder="E-mail"/>
+                   </label>
+                  </div>
+                </div>         
+                </div>
+              </div>
+            </div>
+
+            <div className="forma">
+              <div className="clan">
+                  4. Član (opcionalno)
+              </div>
+              <div className="box-clan">
+                <div className="input">
+                  <div className="unos">
+                    <div className="unos-ikona">
+                      <img src={Ikona6} alt="Ikona1" height={'50px'} />
+                    </div>
+                    <div className="unos-tekst">
+                      <label>
+                      <input type="text" placeholder="Ime i prezime"/>
+                      </label>
+                    </div>
+                  </div>
+                  <div className="unos">
+                  <div className="unos-ikona">
+                    <img src={Ikona2} alt="Ikona1" height={'50px'} />
+                  </div>
+                  <div className="unos-tekst">
+                    <label>
+                    <input type="text" placeholder="Grad"/>
+                    </label>
+                  </div>
+                </div>
+                <div className="unos">
+                  <div className="unos-ikona">
+                    <img src={Ikona1} alt="Ikona1" height={'50px'} />
+                  </div>
+                  <div className="unos-tekst">
+                    <label>
+                    <input type="text" placeholder="Srednja škola"/>
+                    </label>
+                  </div>
+                </div>
+                <div className="unos">
+                  <div className="unos-ikona">
+                    <img src={Ikona5} alt="Ikona1" height={'50px'} />
+                  </div>
+                  <div className="unos-tekst">
+                   <label>
+                   <input type="text" placeholder="Razred"/>
+                   </label>
+                  </div>
+                </div>
+                <div className="unos">
+                  <div className="unos-ikona">
+                    <img src={Ikona4} alt="Ikona1" height={'50px'} />
+                  </div>
+                  <div className="unos-tekst">
+                   <label>
+                   <input type="text" placeholder="Broj telefona"/>
+                   </label>
+                  </div>
+                </div>
+                <div className="unos">
+                  <div className="unos-ikona">
+                    <img src={Ikona3} alt="Ikona1" height={'50px'} />
+                  </div>
+                  <div className="unos-tekst">
+                   <label>
+                   <input type="text" placeholder="E-mail"/>
+                   </label>
+                  </div>
+                </div>         
+                </div>
+              </div>
+            </div>
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+          </div>
+        </div>
+      </div>
+  )
+}
+
+export default prijava
