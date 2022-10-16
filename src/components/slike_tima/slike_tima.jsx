dd67a307386f7d91caf818ac8deec183be834390
IlijaTrifke <gtrifunovic05@gmail.com>
2022-10-17 01:49:38 +0200

    potrebno pozicioniranje

--- src/components/slike_tima/slike_tima.jsx
+++ src/components/slike_tima/slike_tima.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import "./slike_tima.css";
-import SlikaStrelica from "../../images/Slike tima - Strelice oko slike koordinatora.png";
+import StrelaLevo from "../../images/LeftArrow.png";
+import StrelaDesno from "../../images/RightArrow.png";
 import SlikaBordera from "../../images/Slike tima - okvir za koordinatora.png";
+import SlikaKoordinatora from "../../images/5-3.jpg";
+import SLikaBordera1 from "../../images/Slike tima - okvir za sliku tima.png";
 
 const slike_tima = () => {
   return (
@@ -19,17 +22,48 @@ const slike_tima = () => {
       </div>
       <div className="slike-tima-container">
         <div className="slike-tima-levo">
-          <img
-            className="slika-strelica"
-            src={SlikaStrelica}
-            alt="Strelice oko slike koordinatora"
-          />
-          <img
-            className="slika-bordera"
-            src={SlikaBordera}
-            alt="Okvir za koordinatora"
-          />
+          <div className="gornji-deo">
+            <img
+              className="slika-strela-levo"
+              src={StrelaLevo}
+              alt="Strelica leva oko slike koordinatora"
+            />
+            <img
+              className="slika-bordera"
+              src={SlikaBordera}
+              alt="Okvir za sliku koordinatora"
+            />
+            <img
+              className="slika-koordinatora"
+              src={SlikaKoordinatora}
+              alt="Slika koordinatora"
+            />
+            <img
+              className="slika-strela-desno"
+              src={StrelaDesno}
+              alt="Strelica desna oko slike koordinatora"
+            />
+            <div className="opis-slike">
+              <h1 className="ime-koordinatora">Vladislav Vidović</h1>
+              <h3 className="funkcija-koordinatora">Koordinator projekta</h3>
+            </div>
+          </div>
         </div>
+        {/* <div className="slike-tima-desno">
+          <img
+            className="slika-bordera1"
+            src={SLikaBordera1}
+            alt="Okvir za sliku tima"
+          />
+          <p className="opis-tima">
+            Tim za dizajn, u saradnji sa timom za odnose sa javnošću, na
+            kreativan način osmišljava vizuelni identitet projekta. Svoju
+            originalnost i veštine izražava kroz stvaranje objava za društvene
+            mreže, koje za cilj imaju da ti na najzanimljiviji način prenesu
+            informacije o našem takmičenju, kao i izgleda sajta ovogodišnjeg
+            takmičenja.
+          </p>
+        </div> */}
       </div>
     </div>
   );
