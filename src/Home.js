2f830e48a7bcb13143ec2fccda83e51e1c6559aa
petar-graovac <petargraovac01@gmail.com>
2022-10-17 23:50:30 +0200

    faq

--- src/Home.js
+++ src/Home.js
@@ -2,11 +2,12 @@ import React from "react";
 import Animacija from "./components/animacija/animacija.js";
 import Tajmer from "./components/tajmer/tajmer.jsx";
 import O_projektu from "./components/o_projektu/o_projektu.js";
+import Faq from "./components/faq/faq.js";
 
 const Home = () => {
   return (
     <div>
-      <O_projektu />
+      <Faq />
     </div>
   );
 };
--- src/components/o_projektu/o_projektu.js
+++ src/components/o_projektu/o_projektu.js
@@ -8,44 +8,6 @@ const o_projektu = () => {
         <div id="rectangle"></div>
         <div id="tekst_naslov">O PROJEKTU</div>
       </div>
-      <div className="telo">
-        <div className="tekst">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-          perferendis facere odio! Eius omnis, iste dicta numquam ad dolor
-          dignissimos error ea, necessitatibus, veritatis enim obcaecati nisi
-          tempora tenetur? Neque.
-          <br />
-          <br />
-          Optio facere repudiandae nam iusto nobis? Ad voluptatem corrupti
-          consequuntur odit beatae aperiam voluptas tempora illum, eveniet
-          facere repellendus asperiores accusamus nam magnam quo cum quam,
-          nostrum, reprehenderit molestiae saepe. Perspiciatis eum nihil et
-          accusamus hic repellendus nobis provident alias ea doloribus beatae,
-          cumque, autem laborum.
-          <br />
-          <br />
-          Magnam animi neque impedit iure illo nobis, saepe minima ut
-          necessitatibus distinctio consequatur amet? Optio facere repudiandae
-          nam iusto nobis? Ad voluptatem corrupti consequuntur odit beatae
-          aperiam voluptas tempora illum, eveniet facere repellendus asperiores
-          accusamus nam magnam quo cum quam, nostrum, reprehenderit molestiae
-          saepe. Perspiciatis eum nihil et accusamus hic repellendus nobis
-          provident alias ea doloribus beatae, cumque, autem laborum.
-          <br />
-          <br />
-          Optio facere repudiandae nam iusto nobis? Ad voluptatem corrupti
-          consequuntur odit beatae aperiam voluptas tempora illum, eveniet
-          facere repellendus asperiores accusamus nam magnam quo cum quam,
-          nostrum, reprehenderit molestiae saepe. Perspiciatis eum nihil et
-          accusamus hic repellendus nobis provident alias ea doloribus beatae,
-          cumque, autem laborum.
-        </div>
-        <div className="statistika">
-          <div className="timovi"></div>
-          <div className="gradovi"></div>
-          <div className="takmicari"></div>
-        </div>
-      </div>
     </div>
   );
 };
