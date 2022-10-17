f618b7c9ec86cb1a05efe482e3746e70869d09e7
nikola787 <ratkovicnikola64@gmail.com>
2022-10-17 23:50:53 +0200

    ispravljena master grana

--- src/Home.js
+++ src/Home.js
@@ -4,10 +4,7 @@ import Radionice from "./components/radionice/radionice.jsx";
 import Navbar from "./components/navbar/navbar";
 import Tajmer from "./components/tajmer/tajmer.jsx";
 import SlikeTima from "./components/slike_tima/slike_tima.jsx";
-import Prijava from "./components/prijava/Prijava.js";
 import Gradijent from "./components/gradijent/gradijent.js";
-import Navbar from "./components/navbar/navbar";
-import Tajmer from "./components/tajmer/tajmer.jsx";
 import Agenda from "./components/agenda/agenda.jsx";
 import Footer from "./components/footer/footer";
 
