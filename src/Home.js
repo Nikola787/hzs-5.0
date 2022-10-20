c603c12713b115c2708e88eb79bba65bb8562388
IlijaTrifke <gtrifunovic05@gmail.com>
2022-10-21 00:30:30 +0200

    doradjene ispravke

--- src/Home.js
+++ src/Home.js
@@ -4,17 +4,13 @@ import Radionice from "./components/radionice/radionice.jsx";
 import Navbar from "./components/navbar/navbar";
 import Tajmer from "./components/tajmer/tajmer.jsx";
 import SlikeTima from "./components/slike_tima/slike_tima.jsx";
-import Prijava from "./components/prijava/Prijava.js";
 import Gradijent from "./components/gradijent/gradijent.js";
-import Navbar from "./components/navbar/navbar";
-import Tajmer from "./components/tajmer/tajmer.jsx";
 import Agenda from "./components/agenda/agenda.jsx";
 
 const Home = () => {
   return (
     <div>
-      {/* <Animacija /> */}
-      <Gradijent />
+      <Radionice />
     </div>
   );
 };
