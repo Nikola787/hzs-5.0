2b3db2ad1e4ff4aed5e9c0be3513776f97b720b0
Nikola787 <63933389+Nikola787@users.noreply.github.com>
2022-10-22 14:11:27 +0200

    faq dodat

--- src/Home.js
+++ src/Home.js
@@ -7,11 +7,11 @@ import SlikeTima from "./components/slike_tima/slike_tima.jsx";
 import Gradijent from "./components/gradijent/gradijent.js";
 import Agenda from "./components/agenda/agenda.jsx";
 import Footer from "./components/footer/footer";
-import Faq from "./components/faq/faq.js";
+import Faq from "./components/faq/Faq.js";
 import O_projektu from "./components/o_projektu/o_projektu.js";
 import Iskustva from "./components/iskustva/iskustva.js";
 import Partneri from "./components/partneri/partneri.js";
-import PrijavaT from "./components/prijava_tekst/PrijavaT.js"
+import PrijavaT from "./components/prijava_tekst/PrijavaT.js";
 
 const Home = () => {
   return (
@@ -22,11 +22,12 @@ const Home = () => {
       {/* <Tajmer /> */}
       {/* <O_projektu /> */}
       {/* <Iskustva /> */}
-      <Radionice />
+      {/* <Radionice /> */}
+      {/* <Faq /> */}
       {/* <Agenda /> */}
       {/* <SlikeTima /> */}
-      <Partneri />
-      <PrijavaT />
+      {/* <Partneri /> */}
+      {/* <PrijavaT /> */}
       {/* <Footer /> */}
     </div>
   );
