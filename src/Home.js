2913ebc68c87168bc51592b7b0b20df13bb7e41a
Nikola787 <63933389+Nikola787@users.noreply.github.com>
2022-10-24 20:55:09 +0200

    forma

--- src/Home.js
+++ src/Home.js
@@ -2,7 +2,6 @@ import React from "react";
 import Animacija from "./components/animacija/animacija.js";
 import Radionice from "./components/radionice/radionice.jsx";
 import Prijava from "./components/prijava/prijava.js";
-import Gradijent from "./components/gradijent/gradijent.js";
 import Navbar from "./components/navbar/navbar";
 import Tajmer from "./components/tajmer/tajmer.jsx";
 import SlikeTima from "./components/slike_tima/slike_tima.jsx";
