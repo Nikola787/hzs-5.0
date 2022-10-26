bb2a827f79a671d4655891b738a1631989185159
dimitrijeostojic <dimitrije219@gmail.com>
2022-10-27 01:46:10 +0200

    Uradjen footer responsive

--- src/Home.js
+++ src/Home.js
@@ -20,10 +20,10 @@ const Home = () => {
       <Gradijent />
       {/* <Tajmer /> */}
       {/* <O_projektu /> */}
-      <Iskustva /> 
+      <Iskustva />
       <Radionice />
       <Agenda />
-      <Faq />
+      {/* <Faq /> */}
       {/* <SlikeTima /> */}
       <Partneri />
       <PrijavaT />
