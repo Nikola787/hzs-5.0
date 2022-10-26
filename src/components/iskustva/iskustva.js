5017bc2b63b1ccefd59b855250485eb8ed3a7349
Nikola787 <63933389+Nikola787@users.noreply.github.com>
2022-10-26 23:35:42 +0200

    spojene sekcije o pr i iskustva

--- src/components/iskustva/iskustva.js
+++ src/components/iskustva/iskustva.js
@@ -2,15 +2,20 @@ import React from "react";
 import "./iskustva.css";
 import Slika from "./slika.png";
 
+import OP from "../o_projektu/o_projektu.js";
+
 const iskustva = () => {
   return (
-    <div className="iskustva-d">
-      <div className="naslov-i">
-        <div className="naslov-tekst-i">
-          <h1>ISKUSTVA</h1>
+    <div className="dveSekcije">
+      <OP />
+      <div className="iskustva-d">
+        <div className="naslov-i">
+          <div className="naslov-tekst-i">
+            <h1>ISKUSTVA</h1>
+          </div>
         </div>
+        <div className="iskustva-deo"></div>
       </div>
-      <div className="iskustva-deo"></div>
     </div>
   );
 };
