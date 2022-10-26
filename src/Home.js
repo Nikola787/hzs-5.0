becdee96ac07e2725936b29e17c8ab40e4fb568a
Nikola787 <63933389+Nikola787@users.noreply.github.com>
2022-10-26 13:32:23 +0200

    izmena

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,10 @@
 import React from "react";
 import Animacija from "./components/animacija/animacija.js";
 import Radionice from "./components/radionice/radionice.jsx";
-import Navbar from "./components/navbar/navbar";
 import Tajmer from "./components/tajmer/tajmer.jsx";
 import SlikeTima from "./components/slike_tima/slike_tima.jsx";
 import Gradijent from "./components/gradijent/gradijent.js";
 import Agenda from "./components/agenda/agenda.jsx";
-import Footer from "./components/footer/footer";
 import Faq from "./components/faq/faq.js";
 import O_projektu from "./components/o_projektu/o_projektu.js";
 import Iskustva from "./components/iskustva/iskustva.js";
@@ -16,7 +14,7 @@ import PrijavaT from "./components/prijava_tekst/PrijavaT.js";
 const Home = () => {
   return (
     <div>
-      <Navbar />
+      {/* <Navbar /> */}
       <Gradijent />
       {/* <Tajmer /> */}
       {/* <O_projektu /> */}
@@ -24,10 +22,10 @@ const Home = () => {
       <Radionice />
       <Agenda />
       {/* <Faq /> */}
-      <SlikeTima />
+      {/* <SlikeTima /> */}
       <Partneri />
       <PrijavaT />
-      <Footer />
+      {/* <Footer /> */}
 
       {/* <Prijava /> */}
     </div>
