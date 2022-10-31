17ffb40c29cec1c01c5656269d60923153050d18
Nikola787 <63933389+Nikola787@users.noreply.github.com>
2022-10-31 23:09:22 +0100

    pocetak

--- src/components/animacija/animacija.js
+++ src/components/animacija/animacija.js
@@ -5,7 +5,7 @@ import bgVideo from "./video.mp4";
 const animacija = () => {
   return (
     <div className="animacijaD">
-      <video id="video" autoPlay={true} muted>
+      <video id="video" autoPlay={true} muted playsinline type='video/mp4' controls="true">
         <source src={bgVideo} type="video/mp4" />
       </video>
     </div>
