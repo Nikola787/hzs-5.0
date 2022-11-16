13ae42a1b6f410df0149e7612865f2e76da8a980
Nikola787 <63933389+Nikola787@users.noreply.github.com>
2022-11-16 21:17:57 +0100

    animacija

--- src/components/animacija/animacija.js
+++ src/components/animacija/animacija.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./animacija.css";
 import bgVideo from "./video.mp4";
+import videoAvi from "./videoAvi.avi";
 
 const animacija = () => {
   return (
     <div className="animacijaD">
-      <video id="video" controls="false" autoPlay={true} webkit-playsinline playsinline="true" muted type='video/mp4' >
+      {/* <video id="video" autoPlay={true} webkit-playsinline playsinline="true" muted type='video/mp4' >
+        <source src={bgVideo} type="video/mp4" />
+      </video> */}
+      <video id="video" autoPlay={true} muted type='video/mp4' >
         <source src={bgVideo} type="video/mp4" />
       </video>
     </div>
