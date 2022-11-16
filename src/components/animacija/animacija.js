8b6a529d0c74aecfff5b5b8ee451e607de55e39c
Nikola787 <63933389+Nikola787@users.noreply.github.com>
2022-11-16 21:20:29 +0100

    izmena

--- src/components/animacija/animacija.js
+++ src/components/animacija/animacija.js
@@ -6,10 +6,14 @@ import videoAvi from "./videoAvi.avi";
 const animacija = () => {
   return (
     <div className="animacijaD">
-      {/* <video id="video" autoPlay={true} webkit-playsinline playsinline="true" muted type='video/mp4' >
-        <source src={bgVideo} type="video/mp4" />
-      </video> */}
-      <video id="video" autoPlay={true} muted type='video/mp4' >
+      <video
+        id="video"
+        autoPlay={true}
+        webkit-playsinline
+        playsinline="true"
+        muted
+        type="video/mp4"
+      >
         <source src={bgVideo} type="video/mp4" />
       </video>
     </div>
