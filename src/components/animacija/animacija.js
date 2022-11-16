92e8209fe91d158534f91350e2e6b0fd7e8ff1c1
Nikola787 <63933389+Nikola787@users.noreply.github.com>
2022-11-16 21:08:24 +0100

    animacija izmenjena

--- src/components/animacija/animacija.js
+++ src/components/animacija/animacija.js
@@ -5,7 +5,7 @@ import bgVideo from "./video.mp4";
 const animacija = () => {
   return (
     <div className="animacijaD">
-      <video id="video" autoPlay={true} webkit-playsinline playsinline="true" muted type='video/mp4' >
+      <video id="video" controls="false" autoPlay={true} webkit-playsinline playsinline="true" muted type='video/mp4' >
         <source src={bgVideo} type="video/mp4" />
       </video>
     </div>
